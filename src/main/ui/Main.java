f74714d5ffbe70647e66d77eec65e0c662149cc6
Jkites <99958936+Jkites@users.noreply.github.com>
2024-03-07 22:41:24 -0800

    Fixed Comment

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,7 +1,7 @@
 package ui;
 
 public class Main {
-    //inspired by JsonSerializationDemo https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo1
+    //inspired by JsonSerializationDemo https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
     public static void main(String[] args) {
         new HikeApp();
     }
